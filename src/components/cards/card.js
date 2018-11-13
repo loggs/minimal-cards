4bcd8050136499760912f7107448f111156af1a0
suguloga <lcsugit@gmail.com>
2018-11-12 23:37:15 -0800

    Move mouseDown and remove onTouchStart

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import CardFace from "./card-face";
-import { flipCard, mouseDown } from "../../actions/index";
+import { flipCard } from "../../actions/index";
 import { Motion, spring } from "react-motion";
 
 class Card extends Component {
-  handleTouchStart(key, pressLocation, e) {
-    this.props.mouseDown(key, pressLocation, e.touches[0]);
-  }
-
   render() {
     let x;
     let y;
     let style;
     const height = 295;
-    const { uniqueId, data, flipCard, cards, mouseDown } = this.props;
+    const { uniqueId, data, flipCard, cards } = this.props;
     const cardCount = cards.order.length;
     // For each position get the designated height
     const layout = [...Array(cardCount).keys()].map(n => {
@@ -46,8 +42,6 @@ class Card extends Component {
           <div
             className={"flip-container " + extraClass}
             onClick={() => flipCard(uniqueId)}
-            // onMouseDown={() => mouseDown(null, uniqueId, [x, y])}
-            // onTouchStart={this.handleTouchStart.bind(null, uniqueId, [x, y])}
           >
             <div
               className="flipper"
@@ -57,22 +51,8 @@ class Card extends Component {
                 zIndex: uniqueId === cards.lastPressed ? 99 : visualPosition
               }}
             >
-              <CardFace
-                id={uniqueId}
-                side="front"
-                onMouseDown={() => mouseDown(null, uniqueId, [x, y])}
-                onTouchStart={() =>
-                  this.handleTouchStart(null, uniqueId, [x, y])
-                }
-              />
-              <CardFace
-                id={uniqueId}
-                side="back"
-                onMouseDown={() => mouseDown(null, uniqueId, [x, y])}
-                onTouchStart={() =>
-                  this.handleTouchStart(null, uniqueId, [x, y])
-                }
-              />
+              <CardFace id={uniqueId} side="front" />
+              <CardFace id={uniqueId} side="back" />
             </div>
           </div>
         )}
@@ -85,5 +65,5 @@ export default connect(
   ({ cards }) => ({
     cards
   }),
-  { flipCard, mouseDown }
+  { flipCard }
 )(Card);
